2696a27cd9bfdc1a111cbec2b286914a6c8314f1
Haruna Popoola <haruna.popoola@yahoo.com>
2022-01-06 11:27:12 +0100

    fix typo

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <>
-      <h1 className={styles.header}>Welcome to Amocvad</h1>
+      <h1 className={styles.header}>Welcome to Amcovad</h1>
     </>
   );
 }
