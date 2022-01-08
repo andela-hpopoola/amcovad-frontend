c26cef12ad0fd9531326ebafbd27042561394297
Adeyemo Rasheed <71451975+RACHID-WEBDEV@users.noreply.github.com>
2022-01-08 11:46:28 +0100

    #180822214 Create a reusable button component (#4)
    * Create a button component
    
    * Add a prettier config file

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
-import Link from 'next/link';
+import Button from '@/components/form/Button';
 
 export default function Home() {
   return (
     <>
       <h1 className={styles.header}>Welcome to Amcovad</h1>
+
+      <h2>Solid Button</h2>
+      <Button>Sign in</Button>
+
+      <Button color="secondary">try it now</Button>
+
+      <h2>Outline Button</h2>
+
+      <Button outline>Sign in</Button>
+
+      <Button outline color="secondary">
+        Try it now
+      </Button>
     </>
   );
 }
 
 const styles = {
-  header: 'text-3xl font-bold underline text-red-500',
+  header: 'text-3xl font-bold underline text-red-500'
 };
