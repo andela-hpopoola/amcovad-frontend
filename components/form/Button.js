7109fb2b41c52045a8c05fe76e543ba40e31ad95
Adeyemo Rasheed <71451975+RACHID-WEBDEV@users.noreply.github.com>
2022-01-11 14:46:40 +0000

    Update Button.js
    Fix typography

--- components/form/Button.js
+++ components/form/Button.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PropTypes } from 'prop-types';
+import PropTypes from 'prop-types';
 
 const Button = ({ children, color, onClick, outline }) => {
   const outlineClass = outline ? 'outline' : 'solid';
