429e15d4d04989f88af0972b986fb5566bcaec33
Adeyemo Rasheed <71451975+RACHID-WEBDEV@users.noreply.github.com>
2022-01-12 20:35:58 +0100

    #180822332 Integrate a storybook for button component (#9)
    * fix typograpy
    
    * #180822332 Integrate a storybook for button component

--- components/form/Button.js
+++ components/form/Button.js
@@ -1,8 +1,8 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Button = ({ children, color, onClick, outline }) => {
-  const outlineClass = outline ? "outline" : "solid";
+  const outlineClass = outline ? 'outline' : 'solid';
 
   return (
     <button className={`${styles[outlineClass][color]}`} onClick={onClick}>
@@ -11,32 +11,32 @@ const Button = ({ children, color, onClick, outline }) => {
   );
 };
 
-Button.PropTypes = {
+Button.propTypes = {
   className: PropTypes.string,
   color: PropTypes.string,
   onClick: PropTypes.func,
-  outline: PropTypes.bool,
+  outline: PropTypes.bool
 };
 
 Button.defaultProps = {
-  color: "primary",
+  color: 'primary',
   outline: false,
-  onClick: () => {},
+  onClick: () => {}
 };
 
 const styles = {
   solid: {
     primary:
-      "h-10 px-5 m-2 text-white transition-colors uppercase duration-150 bg-[#01A1DF] rounded-md focus:shadow-outline hover:bg-[#5D6677] hover:text-white",
+      'h-10 px-5 m-2 text-white transition-colors uppercase duration-150 bg-[#01A1DF] rounded-md focus:shadow-outline hover:bg-[#5D6677] hover:text-white',
     secondary:
-      "h-10 px-5 m-2 text-white transition-colors uppercase duration-150 bg-[#5D6677] rounded-md focus:shadow-outline hover:bg-[#01A1DF] hover:text-white",
+      'h-10 px-5 m-2 text-white transition-colors uppercase duration-150 bg-[#5D6677] rounded-md focus:shadow-outline hover:bg-[#01A1DF] hover:text-white'
   },
   outline: {
     primary:
-      "h-10 px-5 m-2 text-[#5D6677] uppercase transition-colors duration-150 border border-[#5D6677] rounded-md focus:shadow-outline hover:bg-[#01A1DF] hover:border-[#01A1DF] hover:text-white",
+      'h-10 px-5 m-2 text-[#5D6677] uppercase transition-colors duration-150 border border-[#5D6677] rounded-md focus:shadow-outline hover:bg-[#01A1DF] hover:border-[#01A1DF] hover:text-white',
     secondary:
-      "h-10 px-5 m-2 text-[#01A1DF] uppercase transition-colors duration-150 border border-[#01A1DF] rounded-md focus:shadow-outline hover:bg-[#5D6677] hover:border-[#01A1DF] hover:text-white",
-  },
+      'h-10 px-5 m-2 text-[#01A1DF] uppercase transition-colors duration-150 border border-[#01A1DF] rounded-md focus:shadow-outline hover:bg-[#5D6677] hover:border-[#01A1DF] hover:text-white'
+  }
 };
 
 export default Button;
